376e97b2072b3b03c2392917436d273f758033e4
Vitor Faria <vitorfrancafaria@gmail.com>
2022-03-22 13:59:38 -0700

    drawing random dots fully working, removed bold text from index.html, updating labels fully working

--- script.js
+++ script.js
@@ -6,24 +6,30 @@ canvas.addEventListener('click', handleClick);
 let dotX;
 let dotY;
 let dotDirection;
+const dotRadius = 20;
+
+const canvasMaxXY = 600;
+const canvasMinXY = 0;
 
 let clickX;
 let clickY;
 
-let hits;
-let misses;
-let hitsLabel = document.getElementById('hitsLabel').innerHTML;
-let missesLabel = document.getElementById('missesLabel').innerHTML;
+let hits = 0;
+let misses = 0;
+let hitsLabel = document.getElementById('hitsLabel');
+let missesLabel = document.getElementById('missesLabel');
+
+let onStartPage;
 let gameEnded = false;
 
 let timerID;
+let easyness = 1000;
 
 c.save();
 createElements();
 
 function handleClick(e) {
-    clickX = e.offsetX;
-    clickY = e.offsetY;
+    getMouseCoordinates(e);
     detectClickOnStart();
 }
 
@@ -44,12 +50,54 @@ function createElements() {
     c.font = "50px Arial";
     c.fillStyle = 'white';
     c.fillText('START', 220, 470);
+    onStartPage = true;
 }
 
 function detectClickOnStart() {
-    let clickedStartButton = clickX > 100 && clickX < 500 && clickY >   400 && clickY < 500;
+    let clickedStartButton = clickX > 100 && clickX < 500 && clickY >   400 && clickY < 500 && onStartPage;
+
+    let clickedOnDot = isClickOnDot();
 
     if (clickedStartButton) {
-        startRound();
+        timerID = setInterval(startRound, easyness);
     }
+
+    if (clickedOnDot) {
+        easyness += 5;
+        hits++;
+        hitsLabel.innerHTML = 'HITS: ' + hits;
+    }
+}
+
+function startRound() {
+    setDotPosition();
+    drawDot();
+}
+
+function setDotPosition() {
+    const minXY = 20
+    const maxXY = 580;
+    
+    // TODO random position.
+    dotX = Math.random() * (maxXY - minXY) + minXY;
+    dotY = Math.random() * (maxXY - minXY) + minXY;
+}
+
+function drawDot() {
+    c.clearRect(canvasMinXY,canvasMinXY,canvasMaxXY,canvasMaxXY);
+    c.save();
+    c.beginPath();
+    c.arc(dotX, dotY, dotRadius, 0, 2 * Math.PI);
+    c.fillStyle = 'black';
+    c.fill();
+    c.restore();
+}
+
+function getMouseCoordinates(event) {
+    clickX = event.offsetX;
+    clickY = event.offsetY;
+}
+
+function isClickOnDot() {
+    return Math.sqrt(Math.pow((clickX - dotX), 2) + Math.pow((clickY - dotY), 2)) <= dotRadius;
 }
